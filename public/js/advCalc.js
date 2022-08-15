43dcc36d97612b444e8b23135b5126c8756f8a9e
tarekkhalifa1 <user.tarek@outlook.com>
2022-08-15 18:51:03 +0200

    modified advCalc condition

--- public/js/advCalc.js
+++ public/js/advCalc.js
@@ -5,7 +5,7 @@ function advCalculator() {
     let secondNum = prompt("please enter second number");
     // check if user doesn't enter any number
     if (!firstNum || !secondNum) {
-        alert("please enter valid number");
+        alert("please enter number");
         location.reload();
     }
     // check if user enter invalid number
@@ -13,38 +13,40 @@ function advCalculator() {
         alert("please enter valid number");
         location.reload();
     }
-    var operator = prompt('Enter operator ( +, -, x or / ): ');
-    var result;
 
-    switch (operator) {
-        case '+':
-            result = Number(firstNum) + Number(secondNum);
-            document.write(`<div>${firstNum} + ${secondNum} = ${result} </div>`);
-            break;
+    if (firstNum && secondNum && isNaN(firstNum) == false && isNaN(secondNum) == false) {
+        let operator = prompt('Enter operator ( +, -, x or / ): ');
+        let result;
+        switch (operator) {
+            case '+':
+                result = Number(firstNum) + Number(secondNum);
+                document.write(`<div>${firstNum} + ${secondNum} = ${result} </div>`);
+                break;
 
-        case '-':
-            result = Number(firstNum) - Number(secondNum);
-            document.write(`<div>${firstNum} - ${secondNum} = ${result} </div>`)
-            break;
+            case '-':
+                result = Number(firstNum) - Number(secondNum);
+                document.write(`<div>${firstNum} - ${secondNum} = ${result} </div>`)
+                break;
 
-        case 'x':
-            result = Number(firstNum) * Number(secondNum);
-            document.write(`<div>${firstNum} x ${secondNum} = ${result} </div>`)
-            break;
-        case '*':
-            result = Number(firstNum) * Number(secondNum);
-            document.write(`<div>${firstNum} * ${secondNum} = ${result} </div>`)
-            break;
+            case 'x':
+                result = Number(firstNum) * Number(secondNum);
+                document.write(`<div>${firstNum} x ${secondNum} = ${result} </div>`)
+                break;
+            case '*':
+                result = Number(firstNum) * Number(secondNum);
+                document.write(`<div>${firstNum} * ${secondNum} = ${result} </div>`)
+                break;
 
-        case '/':
-            result = Number(firstNum) / Number(secondNum);
-            document.write(`<div>${firstNum} / ${secondNum} = ${result} </div>`)
-            break;
+            case '/':
+                result = Number(firstNum) / Number(secondNum);
+                document.write(`<div>${firstNum} / ${secondNum} = ${result} </div>`)
+                break;
 
-        default:
-            alert('sorry you entered invalid operator please try again');
-            location.reload();
-            break;
+            default:
+                alert('sorry you entered invalid operator please try again');
+                location.reload();
+                break;
+        }
     }
 } // end of advCalculator
 
